d124a1cc785283756b7d9d971e9862c432959edc
salomaonunes <salomaonunespro@gmail.com>
2022-09-27 14:41:54 -0300

    Test different db hostname

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php 
 // Database configuration    
-define('DB_HOST', 'MySQL_Database_Host'); 
-define('DB_USERNAME', 'MySQL_Database_Username'); 
-define('DB_PASSWORD', 'MySQL_Database_Password'); 
-define('DB_NAME', 'MySQL_Database_Name'); 
+define('DB_HOST', 'db'); 
+define('DB_USERNAME', 'root'); 
+define('DB_PASSWORD', 'haha1212'); 
+define('DB_NAME', 'googleapi'); 
  
 // Google API configuration 
-define('GOOGLE_CLIENT_ID', 'Google_Project_Client_ID'); 
-define('GOOGLE_CLIENT_SECRET', 'Google_Project_Client_Secret'); 
+define('GOOGLE_CLIENT_ID', '430613051012-b16f68n3ord6bksjqe4m9qk7l08trkmd.apps.googleusercontent.com'); 
+define('GOOGLE_CLIENT_SECRET', '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'); 
 define('GOOGLE_OAUTH_SCOPE', 'https://www.googleapis.com/auth/drive'); 
-define('REDIRECT_URI', 'https://www.example.com/google_drive_sync.php'); 
+define('REDIRECT_URI', 'http://localhost/images-upload/google_drive_sync.php'); 
  
 // Start session 
 if(!session_id()) session_start(); 
--- index.php
+++ index.php
@@ -28,10 +28,10 @@ if(!empty($_SESSION['status_response'])){
 <?php } ?>
 
 <div class="col-md-12">
-    <form method="post" action="upload.php" class="form" enctype="multipart/form-data" accept="image/*">
+    <form method="post" action="upload.php" class="form" enctype="multipart/form-data">
         <div class="form-group">
             <label>File</label>
-            <input type="file" name="file" class="form-control">
+            <input type="file" name="file" class="form-control" accept="image/*">
         </div>
         <div class="form-group">
             <input type="submit" class="form-control btn-primary" name="submit" value="Upload"/>
